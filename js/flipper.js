b6e2bce231445bd330f11e6bc45d45e06f13fd40
Jorge Girbau <jorge.girbau@gmail.com>
2015-12-02 16:25:40 -0430

    Small Images
    Smaller images

--- js/flipper.js
+++ js/flipper.js
@@ -2,7 +2,7 @@ $(function() {
 
 	$('.flipper').hover(function() {
 		/* Muestra mensaje */
-		$(this).find('.flipimg').stop().animate({'top':'-100px'}, 200, function(){});
+		$(this).find('.flipimg').stop().animate({'top':'-50px'}, 200, function(){});
 	}, function() {
 		/* Oculta Mensaje */
 		$(this).find('.flipimg').stop().animate({'top':'0px'}, 200, function(){});
